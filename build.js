e79da5e0a0ace2c89a9bb052400f8a07ff2c9b61
Matthew Gibbons <mhgibbons@me.com>
2024-10-28 13:04:46 +0000

    Added path aliases to project and build

--- build.js
+++ build.js
@@ -1,18 +1,22 @@
-// build.js
-import { execSync } from 'child_process';
 import * as esbuild from 'esbuild';
+import { execSync } from 'node:child_process';
+import { resolve } from 'node:path';
 
-execSync('tsc', { stdio: 'inherit' });
+execSync('tsc --emitDeclarationOnly', { stdio: 'inherit' });
 
 await esbuild.build({
-	entryPoints: ['src/index.ts'],
-	outdir: 'dist',
-	bundle: true,
-	platform: 'node',
-	format: 'esm',
-	target: 'esnext',
-	sourcemap: true,
+	alias: {
+		$lib: resolve(import.meta.dirname, './src/lib'),
+		$types: resolve(import.meta.dirname, './src/types')
+	},
 	banner: {
 		js: '// Generated with esbuild'
-	}
+	},
+	bundle: true,
+	entryPoints: ['src/index.ts'],
+	format: 'esm',
+	outdir: 'dist',
+	platform: 'node',
+	sourcemap: true,
+	target: 'esnext'
 });
