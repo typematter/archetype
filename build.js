b19ab9e8b355baa33676bf8504be88c632d68300
Matthew Gibbons <mhgibbons@me.com>
2024-10-28 13:44:00 +0000

    `yaml` is an external dependency

--- build.js
+++ build.js
@@ -1,19 +1,15 @@
 import * as esbuild from 'esbuild';
 import { execSync } from 'node:child_process';
-import { resolve } from 'node:path';
 
 execSync('tsc --emitDeclarationOnly', { stdio: 'inherit' });
 
 await esbuild.build({
-	alias: {
-		$lib: resolve(import.meta.dirname, './src/lib'),
-		$types: resolve(import.meta.dirname, './src/types')
-	},
 	banner: {
 		js: '// Generated with esbuild'
 	},
 	bundle: true,
 	entryPoints: ['src/index.ts'],
+	external: ['yaml'],
 	format: 'esm',
 	outdir: 'dist',
 	platform: 'node',
