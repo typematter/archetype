4fc0d96872229442c4ad76933a3b79da65d2af26
Matthew Gibbons <mhgibbons@me.com>
2024-10-30 05:21:08 +0000

    Path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,13 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	test: {
+		alias: {
+			$lib: '/src/lib',
+			$types: '/src/types'
+		},
+		clearMocks: true,
 		environment: 'node',
-		include: ['src/**/*.test.ts', 'tests/**/*.test.ts']
+		include: ['src/**/*.test.ts', 'tests/**/*.test.ts'],
+		restoreMocks: true
 	}
 });
