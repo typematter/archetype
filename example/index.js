001ac59af5edf41497b810d8b9271957585166b2
Matthew Gibbons <mhgibbons@me.com>
2024-10-30 06:21:15 +0000

    Better example of frontmatter validation

--- example/index.js
+++ example/index.js
@@ -1,7 +1,31 @@
 import { createLocalStore, createValidator } from '@accuser/archetype';
+import { readFileSync } from 'node:fs';
 import { join } from 'node:path';
 import { cwd } from 'node:process';
+import { parse } from 'yaml';
 
 const validator = await createValidator({
 	store: createLocalStore(join(cwd(), 'data', 'archetypes'))
 });
+
+const postArchetype = await validator.loadArchetype('post');
+
+console.log('Post archetype:');
+console.dir(postArchetype, { depth: null });
+console.log();
+
+const post = readFileSync(join(cwd(), 'data', 'posts', 'first-post.md'), 'utf8');
+const frontmatter = parse(post.split('---\n')[1]);
+
+console.log('Frontmatter:');
+console.dir(frontmatter, { depth: null });
+console.log();
+
+const { errors, valid } = await validator.validateFrontmatter(frontmatter);
+
+if (!valid) {
+	console.error('Frontmatter is invalid!');
+	console.error(errors);
+} else {
+	console.log('Frontmatter is valid!');
+}
