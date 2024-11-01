aa73c1e044b8bdff5f2de92f6a6f08ba266c27ac
Matthew Gibbons <mhgibbons@me.com>
2024-11-01 21:39:00 +0000

    Remove archetype handling functions and related tests

--- src/lib/store/archetype-from-yaml.ts
+++
@@ -1,6 +0,0 @@
-import type { Archetype } from '$types/archetype.js';
-import { parse } from 'yaml';
-
-const archetypeFromYaml = (yaml: string): Archetype => parse(yaml);
-
-export default archetypeFromYaml;
